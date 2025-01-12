eb9c90a643846293fb2ca4998a91d3225d7f505a
JocelynVelarde <a01285780@itesm.mx>
2025-01-11 21:35:31 -0600

    created register user post method

--- app.py
+++ app.py
@@ -10,5 +10,19 @@ def get_status():
     else:
         print(response.status_code, response.json())
 
+# POST register user on the books API to obtain bearer token
+def register_user(client_mail, client_name):
+    data = {
+        "clientName": client_name,
+        "clientEmail": client_mail
+    }
+    response = requests.post(base_url + '/api-clients', json=data)
+    if response.status_code == 201:
+        print(response.json())
+    else:
+        print(response.status_code, response.json())
+
+
 if __name__ == '__main__':
     get_status()
+    register_user("honeybeeboo@gmail.com", "Jocelyn")
